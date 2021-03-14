6085425b4dcdb3e82092d52338d13e3845009b77
Rob <robert.balog000@gmail.com>
2021-03-14 15:08:41 +0200

    -implemented test cases

--- tweet_lite/urls.py
+++ tweet_lite/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from tweets.views import (
     home_view,
     tweet_detail_view,
     tweet_list_view,
     tweet_create_view,
+    tweet_delete_view,
+    tweet_action_view,
 )
 
 urlpatterns = [
@@ -13,5 +15,6 @@ urlpatterns = [
     path('', home_view),
     path('create-tweet/', tweet_create_view),
     path('tweets/', tweet_list_view),
-    path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('tweets/<int:tweet_id>/', tweet_detail_view),
+    path('api/tweets/', include('tweets.urls'))
 ]
--- tweets/models.py
+++ tweets/models.py
@@ -5,12 +5,22 @@ import random
 User = settings.AUTH_USER_MODEL
 
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class Tweet(models.Model):
     # id = models.AutoField(primary_key
     # many users can have many tweets
+    parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(
+        User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-id']
@@ -18,9 +28,6 @@ class Tweet(models.Model):
     def __str__(self):
         return self.content
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "content": self.content,
-            "likes": random.randint(0, 200)
-        }
+    @property
+    def is_retweet(self):
+        return self.parent != None
