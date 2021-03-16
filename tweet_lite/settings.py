d906867c38a580761dadbf4a7a97114e1cb29c81
Rob <robert.balog000@gmail.com>
2021-03-16 17:12:32 +0200

    -functional login/logout/register

--- tweet_lite/settings.py
+++ tweet_lite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # internal
     'tweets',
+    'accounts',
     # third party
     'rest_framework',
     'corsheaders',
--- tweet_lite/urls.py
+++ tweet_lite/urls.py
@@ -3,6 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+
+from accounts.views import (
+    login_view,
+    logout_view,
+    register_view
+)
 from tweets.views import (
     tweets_list_view,
     tweets_detail_view,
@@ -12,10 +18,14 @@ from tweets.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', tweets_list_view),
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('register/', register_view),
     path('<int:tweet_id>', tweets_detail_view),
     path('profile/<str:username>', tweets_profile_view),
     path('api/tweets/', include('tweets.api.urls'))
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
